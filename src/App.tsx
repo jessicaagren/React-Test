fd30254ea52f053d77a4dceb69aac32575b70460
jessicaagren <jessicaagren@hotmail.com>
2025-02-13 14:37:53 +0100

    Started project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import './index.scss';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
